32a6df59cfc1302ee37cdd9bb314ae42d39b66d6
Ethan <ashin0709@gmail.com>
2022-06-15 00:24:37 -0400

    update

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Consultant on 6/14/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let imageToload = selectedImage{
+            imageView.image = UIImage(named: imageToload)
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
